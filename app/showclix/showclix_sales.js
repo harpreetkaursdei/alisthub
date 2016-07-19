79329d1a6ef181c42d760356b2836c9c4e081c4f
Your Name <you@example.com>
2016-07-19 14:27:09 +0530

    updated the graph code and css

--- app/showclix/showclix_sales.js
+++ app/showclix/showclix_sales.js
@@ -8,17 +8,32 @@ module.exports = function()
     
   this.getSalesData = function(req,res,next)
   {
+    console.log(req);
+
+    var reportDataUrl = "https://api.showclix.com/Sale/search?seller = " + req.showclix_seller_id;
+
+    if(req.end_date && req.start_date){
+      reportDataUrl += "&end_date=" + req.end_date + " &start_date="+ req.start_date;     
+    }
+
+    //"https://api.showclix.com/Sale/search?end_date=" + req.end_date + " &start_date="+ req.start_date + " &seller="+ req.showclix_seller_id ;
 
     request.get({
-                headers: {"content-type": "application/json",'X-API-Token':req.showclix_token}, //{'X-API-Token':req.showclix_token},
-                url:     "https://api.showclix.com/Sale/search",
-                form:    {} }, function(error, response, body){
-                  console.log("====================");
-                  console.log(response.headers);
-                  console.log("====================");
-                  console.log(response.body);
-                  console.log("====================");
+                headers: {'Content-Type':'application/json','X-API-Token': req.showclix_token }, //{'X-API-Token':req.showclix_token},
+                url: reportDataUrl,
+                form: {} }, function(error, response, body) {
+                  //console.log("====================");
+                  //console.log(error);
+                  //console.log(response.body);
+                  //console.log("====================");
+                  //console.log(response.statusCode);
+                  //console.log("====================");
+                  //console.log(body);
+                  if(response.statusCode == 200) {
                   return next({status:1,data:response.body});
+                } else {
+                  return next({status:0,data:""});
+                }
     });
   }
  
