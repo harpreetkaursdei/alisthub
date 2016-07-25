3cc28d424108ab29a1103a2ea35c8afcd00ad79e
Deepak khokhar <khokhard@smartdatainc.net>
2016-07-25 10:42:23 +0530

    report changes

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -705,9 +705,9 @@ var webservices = {
     "getSalesData" : "/report/getSalesData" ,
     
     //Signup seller
-    "signupSeller" : "/profile/signup_seller"  
+    "signupSeller" : "/profile/signup_seller",  
 
-    "getReportEvent": "/report/getReportEvent",
+    "getReportEvent": "/report/getReportEvent"
 
 
 
